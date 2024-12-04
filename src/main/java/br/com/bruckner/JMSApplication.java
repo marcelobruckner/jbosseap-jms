f17287ed152441d2a95ddc75ad48678741cadd06
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-12-04 15:20:24 -0300

    Ajustes no projeto

--- src/main/java/br/com/bruckner/JMSApplication.java
+++
@@ -1,9 +0,0 @@
-package br.com.bruckner;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JMSApplication extends Application {
-    
-}
